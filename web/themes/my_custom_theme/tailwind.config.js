68d5e5c1427e9cbd37f58fdc5b0addf1ff08f910
Arinze Madu <arinzemadu@Arinzes-MacBook-Pro.local>
2023-02-27 10:19:20 +0000

    git ignore file updated

---
+++ web/themes/my_custom_theme/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./templates/**/*.html.twig', './src/**/*.php'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
